f3562cca1ef9bc47ba2ed3bd8d539b0e7625f1c7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-10 23:09:55 -0300

    feat: implement function from camera

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,25 +1,57 @@
-import { useState } from 'react';
-import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
+import { useState, useEffect, useRef } from "react";
+import { Image, SafeAreaView, ScrollView, TextInput, View } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
+import { PositionChoice } from "../components/PositionChoice";
 
-import { Header } from '../components/Header';
-import { Button } from '../components/Button';
-import { PositionChoice } from '../components/PositionChoice';
-
-import { styles } from './styles';
-import { POSITIONS, PositionProps } from '../utils/positions';
+import { styles } from "./styles";
+import { POSITIONS, PositionProps } from "../utils/positions";
 
 export function Home() {
-  const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [positionSelected, setPositionSelected] = useState<PositionProps>(
+    POSITIONS[0]
+  );
+  const [photoURI, setPhotoURI] = useState<null | string>(null);
+
+  const cameraRef = useRef<Camera>(null);
+
+  const handleTakePicture = async () => {
+    const photo = await cameraRef.current.takePictureAsync();
+    setPhotoURI(photo.uri)
+  };
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync().then((response) =>
+      setHasCameraPermission(response.granted)
+    );
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={styles.scroll}
+        showsVerticalScrollIndicator={false}
+      >
         <View>
           <Header position={positionSelected} />
 
           <View style={styles.picture}>
-
-            <Image source={{ uri: 'https://github.com/rodrigorgtic.png' }} style={styles.camera} />
+            {hasCameraPermission && !photoURI ? (
+              <Camera
+                ref={cameraRef}
+                type={CameraType.front}
+                style={styles.camera}
+              />
+            ) : (
+              <Image
+                source={{
+                  uri: photoURI ? photoURI : "https://filestore.community.support.microsoft.com/api/images/354ad963-3130-4924-b870-71a46b54fb4a?upload=true",
+                }}
+                style={styles.camera}
+              />
+            )}
 
             <View style={styles.player}>
               <TextInput
@@ -35,7 +67,7 @@ export function Home() {
           positionSelected={positionSelected}
         />
 
-        <Button title="Compartilhar" />
+        <Button title="Compartilhar" onPress={handleTakePicture} />
       </ScrollView>
     </SafeAreaView>
   );
